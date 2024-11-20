9327eb2024654940167565c53fe6e1f743e6abe2
Laamorot <laamorot@etsinf.upv.es>
2024-11-21 00:22:06 +0100

    BSnode

---
+++ BSNode.h
@@ -0,0 +1,36 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+        // miembros públicos
+        T elem;
+        BSNode<T>* left;
+        BSNode<T>* right;
+
+        	
+
+        BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr) : elem(elem), left(left), right(right) {}
+
+        friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn) {
+            out << "Node(" << bsn.elem;
+            if (bsn.left != nullptr) {
+                out << ", left: " << *bsn.left;
+            } else {
+                out << ", left: null";
+            }
+            if (bsn.right) {
+                out << ", right: " << *bsn.right;
+            } else {
+                out << ", right: null";
+            }
+            out << ")";
+            return out;
+        }
+    
+};
+
+#endif
